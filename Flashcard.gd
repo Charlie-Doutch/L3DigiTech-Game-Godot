5fdb62d1334554b48ddd521aa6be972ccaeed645
Charlie-Doutch <cadoutch@student.christscollege.com>
2025-06-05 15:32:23 +1200

    5/06/25
    I made some changes to the flashcard system with the positioning and such of the stuff, however it didn't really work as I expected so I will have to revisit that and change up some of the code and node types for the flashcard system. I also created a new scene for the space invaders game portion of the game so I will have that set up for when I need it.

---
+++ Flashcard.gd
@@ -0,0 +1,81 @@
+extends MarginContainer
+
+# Dictionary of words and their japanese counterparts
+var words1 = {
+	"Hello": "こんにちは",
+	"You": "あなた",
+	"Summer": "なつ",
+	"Milk": "ミルク"
+}
+
+var words2 = {
+	"こんにちは": "hello",
+	"あなた": "you",
+	"なつ": "summer",
+	"ミルク": "milk"
+}
+
+var words = {}
+var current_word = ""
+
+# Called when the node enters the scene tree
+func _ready():
+	# Connect the confirm and submit buttons
+	$ConfirmButton.pressed.connect(_on_confirm_pressed)
+	$SubmitButton.pressed.connect(_on_submit_pressed)
+	
+	# Initially hide quiz UI until list is selected
+	$SubmitButton.visible = false
+	$QuestionLabel.visible = false
+	$AnswerInput.visible = false
+	$ResultLabel.visible = false
+
+func _on_confirm_pressed():
+	var SelectedList = $ListSelect.text
+	
+	if SelectedList == "1":
+		words = words1
+	elif SelectedList == "2":
+		words = words2
+	else:
+		$ResultLabel.text = "Invalid list number. Enter 1 or 2."
+		$ResultLabel.visible = true
+		return
+		
+	# Hide selection UI
+	$ListSelect.visible = false
+	$ConfirmButton.visible = false
+
+	# Show quiz UI
+	$SubmitButton.visible = true
+	$QuestionLabel.visible = true
+	$AnswerInput.visible = true
+	$ResultLabel.visible = true
+
+	ask_question()
+
+
+# Function to ask a new word question
+func ask_question():
+	var word_keys = words.keys()
+	var random_index = randi() % word_keys.size()
+	current_word = word_keys[random_index]
+	
+	# Update UI
+	$QuestionLabel.text = "%s" % current_word
+	$AnswerInput.text = ""
+	$ResultLabel.text = ""
+
+# Called when the submit button is pressed
+func _on_submit_pressed():
+	var user_answer = $AnswerInput.text.strip_edges().to_lower()
+	var correct_answer = words[current_word]
+	
+	if user_answer == correct_answer:
+		$ResultLabel.text = "Correct!"
+	else:
+		$ResultLabel.text = "Wrong! Correct answer: %s" % correct_answer
+	
+	# Ask next question after a short delay
+	await get_tree().create_timer(3).timeout
+	ask_question()
