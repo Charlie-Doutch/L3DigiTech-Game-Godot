0c2226869d55754fa227b7417a0749ce49d8343a
Charlie-Doutch <cadoutch@student.christscollege.com>
2025-06-11 10:00:40 +1200

    11/06/25
    I created some more placeholder enemies and I created a resource script so that I can edit the points, sprite size and other important things for the enemy sprites very easily. This will also probably let me change enemy stats easily in the future.

--- Scenes/enemy_spawn.gd
+++ Scenes/enemy_spawn.gd
@@ -1,3 +1,16 @@
 extends Node2D
 
 class_name EnemySpawn
+
+# configuration/way the enemies spawn
+const ROWS = 5
+const COLUMNS = 10
+const HORIZONTAL_SPACING = 30
+const VERTICAL_SPACING = 30
+const ENEMY_HEIGHT = 20
+const START_Y_POS = -50
+const ENEMY_X_POS_INCREMENT = 10
+const ENEMY_Y_POS_INCREMENT = 20
+
+var movement_direction = 1
+ 
